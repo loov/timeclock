4ef17bc635874534b6474f73384bcabb403bf9b2
Egon Elbre <egonelbre@gmail.com>
2015-11-14 16:47:40 +0200

    Add primitive project view.

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/loov/timeclock/project"
 )
 
 var (
@@ -28,6 +32,49 @@ func main() {
 	http.ListenAndServe(*addr, nil)
 }
 
+func internalError(w http.ResponseWriter, r *http.Request, err error) {
+	message := template.HTMLEscapeString(err.Error())
+	page := fmt.Sprintf(`
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Timeclock</title>
+	<link rel="stylesheet" href="/assets/css/main.css">
+</head>
+<body>
+	<div class="error">%s</div>
+</body>
+</html>
+`, message)
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte(page))
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Printf("error parsing template: %v", err)
+		internalError(w, r, err)
+		return
+	}
+
+	example := &project.Project{
+		Title:    "Alpha",
+		Customer: "ACME",
+		Pricing: project.Pricing{
+			Hours: 480,
+			Price: 1000,
+		},
+		Description: "Implement views",
+		Status:      project.InProgress,
+	}
+
+	err = t.Execute(w, example)
+	if err != nil {
+		log.Printf("error parsing template: %v", err)
+		internalError(w, r, err)
+		return
+	}
 }
