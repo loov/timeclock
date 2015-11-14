c420b57de35a12d2ce78c63cf24c030927bccb5b
Egon Elbre <egonelbre@gmail.com>
2015-11-14 16:02:21 +0200

    Basic server setup.

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"net/http"
+	"os"
+)
 
+var (
+	addr = flag.String("listen", ":8000", "http server `address`")
+)
+
+func main() {
+	flag.Parse()
+
+	host, port := os.Getenv("HOST"), os.Getenv("PORT")
+	if host != "" || port != "" {
+		*addr = host + ":" + port
+	}
+
+	assets := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", assets))
+
+	http.HandleFunc("/", index)
+
+	http.ListenAndServe(*addr, nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
 }
