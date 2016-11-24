50a9225a3a635c2a3a785fb758ba89faede3df92
Egon Elbre <egonelbre@gmail.com>
2016-11-24 14:23:11 +0200

    Add logic to summarize by day.

--- work/summary.go
+++ work/summary.go
@@ -10,15 +10,21 @@ type Summary struct {
 	Durations  map[string]time.Duration
 }
 
+func NewSummary() *Summary {
+	summary := &Summary{}
+	summary.Durations = make(map[string]time.Duration)
+	return summary
+}
+
 func (summary *Summary) Include(activity Activity) error {
 	if activity.Incomplete() {
 		return ErrActivityIncomplete
 	}
 
-	if summary.Start.After(activity.Start) {
+	if summary.Start.IsZero() || summary.Start.After(activity.Start) {
 		summary.Start = activity.Start
 	}
-	if summary.Finish.Before(activity.Finish) {
+	if summary.Finish.IsZero() || summary.Finish.Before(activity.Finish) {
 		summary.Finish = activity.Finish
 	}
 
@@ -29,14 +35,30 @@ func (summary *Summary) Include(activity Activity) error {
 }
 
 func SummarizeActivities(activities []Activity) (*Summary, error) {
-	summary := &Summary{}
-	summary.Durations = make(map[string]time.Duration)
-
+	summary := NewSummary()
 	for _, activity := range activities {
 		if err := summary.Include(activity); err != nil {
 			return nil, err
 		}
 	}
-
 	return summary, nil
+}
+
+func SummarizeActivitiesByDay(activities []Activity, loc *time.Location) (map[time.Time]*Summary, error) {
+	summaries := map[time.Time]*Summary{}
+	for _, activity := range activities {
+		day := activity.Start.In(loc).Truncate(24 * time.Hour)
+
+		summary, ok := summaries[day]
+		if !ok {
+			summary = NewSummary()
+			summaries[day] = summary
+		}
+
+		if err := summary.Include(activity); err != nil {
+			return nil, err
+		}
+	}
+
+	return summaries, nil
 }
